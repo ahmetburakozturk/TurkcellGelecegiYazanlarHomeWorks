dc567b77ad9bfafebcbc17e5baea5ecfc6341d42
Ahmet Burak Ozturk <79537376+ahmetburakozturk@users.noreply.github.com>
2022-04-06 23:48:30 +0300

    MVC homework

---
+++ CoronaParty/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CoronaParty.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoronaParty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PartyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PartyForm( PartyAnswer partyAnswer)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("PartyForm");
+            }
+            else
+            {
+                if (partyAnswer.State == "Evet")
+                {
+                    return View("Thanks");
+                }
+                else
+                {
+                    return View("Sorry");
+                }
+            }
+            
+        }
+    }
+}
---
+++ CoronaParty/Models/PartyAnswer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoronaParty.Models
+{
+    public class PartyAnswer
+    {
+        [Required(ErrorMessage = "Boş Bırakılamaz!")]
+        [Display(Name = "İsim - Soyisim ")]
+        [MinLength(2,ErrorMessage = "En az 2 karakter girilmelidir.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Telefon Numarası ")]
+        public int PhoneNumber { get; set; }
+
+        [Display(Name = "Katılacak Mısınız ?")]
+        [Required(ErrorMessage = "Boş Bırakılamaz.")]
+        public string State { get; set; }
+    }
+}
